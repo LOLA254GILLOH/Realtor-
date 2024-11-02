ad4ed0d0c16c7831f69b2613191f41785aa64feb
LOLA254GILLOH <gilbertoluoko49@gmail.com>
2024-11-02 13:54:54 +0300

    Create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -15,13 +15,17 @@ import {
     FaParking,
     FaChair,
   } from "react-icons/fa";
+  import { getAuth } from "firebase/auth";
+import Contact from '../components/Contact';
 
 export default function Listing() {
+    const auth = getAuth();
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     const [shareLinkCopied, setShareLinkCopied] = useState(false);
+    const [contactLandlord, setContactLandlord] = useState(false);
 
     useEffect(()=> {
         async function fetchListing(){
@@ -120,6 +124,19 @@ export default function Listing() {
                         {listing.furnished ? "Furnished" : "Not furnished"}
                     </li>   
                 </ul>
+                {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+                    <div className="mt-6">
+                        <button
+                            onClick={() => setContactLandlord(true)}
+                            className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+                        >
+                            Contact Landlord
+                        </button>
+                    </div>
+                )}
+                {contactLandlord && (
+                    <Contact userRef={listing.userRef} listing={listing} />
+                )}
             </div>
             <div className="bg-blue-300 w-full h-[200px] lg-[400px]"></div>
         </div>
